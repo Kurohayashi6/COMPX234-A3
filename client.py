ab1c1ddf58141e2dd45ab1dde55a4e8ca333f554
Kurohayashi6 <2011169623@qq.com>
2026-04-21 11:07:37 +0800

    Receive all function, prevent missing bytes.

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+
+def recvall(sock, n):
+    data = bytearray()
+    while len(data) < n:
+        last = sock.recv(n - len(data))
+        if not last:
+            return None
+        data.extend(last)
+    return data.decode('utf8')
+
+
--- server.py
+++ server.py
@@ -16,3 +16,15 @@ sta = {
 
 lock = threading.Lock()
 
+
+def recvall(sock, n):
+    data = bytearray()
+    while len(data) < n:
+        last = sock.recv(n - len(data))
+        if not last:
+            return None
+        data.extend(last)
+    return data.decode('utf8')
+
+
+
